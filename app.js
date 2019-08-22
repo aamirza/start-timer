a1d0d100e001b7cb50b953f801d8b0b9351f912f
Aamir Mirza <aamirshahed@hotmail.com>
2019-08-21 23:25:49 -0400

    Add the ability to timer to countdown and countup.

---
+++ app.js
@@ -0,0 +1,5 @@
+const timer = new Timer();
+
+document.querySelector("#main-button").addEventListener("click", (event) => {
+    timer.mainButtonPress(event);
+})
--- timer.js
+++ timer.js
@@ -1,29 +1,84 @@
+class Timer {
+    constructor() {
+        // What would be the timer's "properties"?
+        // The website has loaded. What does the timer do at first?
+        // Well, at first the timer is paused, correct?
+        this.countDown = false;
+        this.countUp = false;
+        // It is also currently on break? Not necessarily.
+        this.shortBreak = false;
+        this.longBreak = false;
+        this.working = false;
+        this.pause = true;
+        // What is it currently set to? It's a dynamic value?
+        this.timerBody = document.querySelector("#timer");
+        this.formValues = document.querySelector("#work-params");
+        this.mainButton = document.querySelector("#main-button");
+    }
 
-function setTimer() {
-    const timer = document.querySelector("#timer");
-    const formValues = document.querySelector("#work-params");
+    get workTime() {
+        return document.querySelector("#work").value;
+    }
 
-    const work = formValues[0].value;
+    get shortBreakTime() {
+        return document.querySelector("#sbreak").value;
+    }
 
-    timer.innerHTML = convertToTime(work*60);
-}
+    get secondsRemaining() {
+        return this.timerBody
+    }
 
-function convertToTime(timeInSeconds) {
-    function pad(number) {
-        if (number < 10) {
-            return "0" + number;
+    set timerText(seconds) {
+        if (seconds >= 0) {
+            this.countDown = true;
+            this.countUp = false;
+            this.timerBody.innerHTML = this.convertToTime(seconds);
         } else {
-            return number;
+            this.countUp = true;
+            this.countDown = false;
+            this.timerBody.innerHTML = "+" + this.convertToTime(seconds*-1);
         }
     }
 
-    const h = Math.floor(timeInSeconds / 3600);
-    const mm = pad(Math.floor(timeInSeconds / 60) - (60*h));
-    const ss = pad(timeInSeconds % 60);
+    set mainButtonText(text) {
+        this.mainButton.textContent = text;
+    }
 
-    return (h ? `${h}:${mm}:${ss}` : `${mm}:${ss}`);
+    convertToTime(timeInSeconds) {
+        function pad(number) {
+            if (number < 10) {
+                return "0" + number;
+            } else {
+                return number;
+            }
+        }
+
+        const h = Math.floor(timeInSeconds / 3600);
+        const mm = pad(Math.floor(timeInSeconds / 60) - (60*h));
+        const ss = pad(timeInSeconds % 60);
+
+        return (h ? `${h}:${mm}:${ss}` : `${mm}:${ss}`);
+    }
+
+    startTimer() {
+        let secondsLapsed = 0;
+        setInterval(function() {
+            if (!this.pause) {
+                secondsLapsed += 1;
+                timer.timerText = (timer.workTime*60) - secondsLapsed;
+            }
+        }, 1000)
+    }
+
+    mainButtonPress(buttonEvent) {
+        const command = buttonEvent.target.textContent.toLowerCase();
+        if (command === "start") {
+            this.timerText = this.workTime * 60;
+            this.working = true;
+            this.pause = false;
+            this.countDown = true;
+            this.mainButtonText = "Stop";
+            this.startTimer();
+        }
+    }
 }
-
-function startCountdown(
-
-)
