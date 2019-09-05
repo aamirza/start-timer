3e7419fe8ef90c2fcc2258e2f0f807ba7dd4b61f
Aamir Mirza <aamirshahed@hotmail.com>
2019-09-04 20:41:43 -0400

    Added the ability to add rows to the work table

--- Table.js
+++ Table.js
@@ -1,5 +1,31 @@
 class Table {
-    constructor() {
-        
+    constructor(tableElement) {
+        this.table = tableElement;
+    }
+
+    removeNoWorkRow() {
+        const noWorkRow = document.querySelector("#no-work-recorded");
+        if (noWorkRow) {
+            noWorkRow.parentElement.removeChild(noWorkRow);
+        }
+    }
+
+    createCell(className="", innerHtml="") {
+        const cell = document.createElement("td");
+        if (className) cell.setAttribute("class", className)
+        if (innerHtml) cell.innerHTML = innerHtml;
+        return cell;
+    }
+
+    addWorkRow(startTime, workLength, breakLength) {
+        const tableRow = document.createElement("tr");
+        tableRow.setAttribute("class", "work-column")
+        const startTimeRow = this.createCell("start-time", startTime);
+        const workLengthRow = this.createCell("work-time", workLength);
+        const breakLengthRow = this.createCell("break-time", breakLength);
+        tableRow.appendChild(startTimeRow);
+        tableRow.appendChild(workLengthRow);
+        tableRow.appendChild(breakLengthRow);
+        this.table.appendChild(tableRow);
     }
 }
