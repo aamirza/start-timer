c943d0e940ef507c7b5fdc71aa32ad9360c60e8e
Aamir Mirza <aamirshahed@hotmail.com>
2019-09-05 20:52:56 -0400

    Calculate totals rows

--- Table.js
+++ Table.js
@@ -1,6 +1,7 @@
 class Table {
     constructor(tableElement) {
         this.table = tableElement;
+        this.totalsRow = document.querySelector(".footer-col");
     }
 
     removeNoWorkRow() {
@@ -27,5 +28,27 @@ class Table {
         tableRow.appendChild(workLengthRow);
         tableRow.appendChild(breakLengthRow);
         this.table.appendChild(tableRow);
+    }
+
+    calculateTotalsRow() {
+        if (!this.totalsRow) {
+            const footerRow = document.createElement("tfoot");
+            footerRow.setAttribute("class", "footer-col")
+            const firstCell = document.createElement("td");
+            firstCell.setAttribute("id", "total-text");
+            firstCell.innerHTML = "Total: ";
+            const totalWork = document.createElement("td");
+            totalWork.setAttribute("id", "total-work");
+            const totalBreak = document.createElement("td");
+            totalBreak.setAttribute("id", "total-break");
+            footerRow.appendChild(firstCell);
+            footerRow.appendChild(totalWork);
+            footerRow.appendChild(totalBreak);
+            this.table.appendChild(footerRow);
+            this.totalsRow = document.querySelector(".footer-col");
+        }
+        const allWorkColumns = document.getElementsByClassName(".work-time");
+        const allBreakColumns = document.getElementsByClassName(".break-time");
+
     }
 }
