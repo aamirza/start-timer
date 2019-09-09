329c6c21d306a5fe884f19b5f1e13484773d9165
Aamir Mirza <aamirshahed@hotmail.com>
2019-09-08 20:47:13 -0400

    Added colours to some of the buttons

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const timer = new Timer();
 
-document.querySelector("#main-button").addEventListener("click", (event) => {
+document.querySelector(".main-button").addEventListener("click", (event) => {
     timer.mainButtonPress(event);
 })
 
-document.querySelector("#second-button").addEventListener("click", (event) => {
+document.querySelector(".second-button").addEventListener("click", (event) => {
     timer.secondButtonPress(event)
 })
