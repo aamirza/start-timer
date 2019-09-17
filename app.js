876fa4979ae846a5c601db60aae60e61f62a7b96
Aamir Mirza <aamirshahed@hotmail.com>
2019-09-16 20:30:35 -0400

    Added task pane (incomplete)

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const timer = new Timer();
 
 document.querySelector(".main-button").addEventListener("click", (event) => {
     timer.mainButtonPress(event);
-})
+});
 
 document.querySelector(".second-button").addEventListener("click", (event) => {
     timer.secondButtonPress(event)
-})
+});
+
+document.querySelectorAll(".view").forEach((element) => {
+    element.addEventListener("click", (event) => {
+        document.querySelectorAll(".view").forEach((paneButton) => {
+            paneButton.classList.remove("selected-pane");
+        });
+        event.target.classList.add("selected-pane");
+        const selectedPaneId = event.target.id.split("-")[0] + "-pane";
+        const panes = document.querySelectorAll(".pane");
+        panes.forEach(pane => pane.classList.remove("current-pane"))
+        document.querySelector("#"+selectedPaneId).classList.add("current-pane");
+    });
+});
